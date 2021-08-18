b47c12141e1c475911169f7cdf3ca408dfc4d8cc
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-18 15:37:06 +0530

    dOWNLOADING FOR MULTIPLE RESOLUTIONS

--- Advanced/movie.py
+++ Advanced/movie.py
@@ -4,19 +4,25 @@ import pytube
 import time
 import datetime
 
-links=["https://www.youtube.com/watch?v=EOC2XOb8efw", "https://www.youtube.com/watch?v=5xNe2e12MRA"]
+links=["https://www.youtube.com/watch?v=10RnRpMj9Mg", "https://www.youtube.com/watch?v=bADu51CTF8g"]
 destination = "/home/sangee/Videos"
 
 def downloadVideos(links):
+    resolutions = ["1080p", "720p", "480p", "360p"]
     for link in links:
         yt = pytube.YouTube(link)
-        stream = yt.streams.filter(res="1080p").first()
+        for resolution in resolutions:
+            stream = yt.streams.filter(res=resolution).first()
+            if stream is not None:
+                break;
         startTime = time.time();
         print("Start Time for Download Link "+link+" "+str(datetime.datetime.now()))
-        print("Download Started "+link)
+        print("Download Started ".center(50,"*"))
         stream.download(output_path = destination)
         finishTime = time.time();
-        print("Download finished "+link)
+        print("Download finished ".center(50,"*"))
         print("Finish Time for Download Link "+link+" "+str(datetime.datetime.now()))
-        print("Time taken for downloading "+link+" is "+str(finishTime-startTime))
+        interval = finishTime-startTime
+        duration = str(round(interval/60,3)) +" Minutes " +str(round(interval%60,3))+ " Seconds"
+        print(("Time taken for downloading "+link+" is "+duration).center(50,"*"))
 downloadVideos(links)
