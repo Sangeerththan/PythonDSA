bc4ab0c65f21d7217d731ef0a992e46075dc1491
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-18 13:29:52 +0530

    movie destination added

--- Advanced/movie.py
+++ Advanced/movie.py
@@ -1,7 +1,8 @@
 #pip3 install pytube
 
 import pytube
-link="https://www.youtube.com/watch?v=0T_1RrlaeJo"
+link="https://www.youtube.com/watch?v=gR-tJhoO4i0&list=PL4QNnZJr8sRNiuUBUvMJ7b4eQJotYO2mo"
+destination = "/home/sangee/Videos"
 yt = pytube.YouTube(link)
 stream = yt.streams.first()
-stream.download()
+stream.download(output_path = destination)
