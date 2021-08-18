42b74630e6f61291c774e201a61977eb57a7c8f1
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-08-18 14:13:25 +0530

    calculating downlaoad duration

--- Advanced/movie.py
+++ Advanced/movie.py
@@ -1,6 +1,9 @@
 #pip3 install pytube
 
 import pytube
+import time
+import datetime
+
 links=["https://www.youtube.com/watch?v=EOC2XOb8efw", "https://www.youtube.com/watch?v=5xNe2e12MRA"]
 destination = "/home/sangee/Videos"
 
@@ -8,6 +11,12 @@ def downloadVideos(links):
     for link in links:
         yt = pytube.YouTube(link)
         stream = yt.streams.filter(res="1080p").first()
+        startTime = time.time();
+        print("Start Time for Download Link "+link+" "+str(datetime.datetime.now()))
+        print("download Started "+link)
         stream.download(output_path = destination)
+        finishTime = time.time();
+        print("download finished "+link)
+        print("Start Time for Download Link "+link+" "+str(datetime.datetime.now()))
+        print("Time tken for downlaoding "+link+" is "+str(startTime - finishTime))
 downloadVideos(links)
-
