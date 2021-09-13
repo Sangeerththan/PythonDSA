78a3361bd9991cee1f79c8041a6d4623b434058a
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-09-13 19:32:08 +0530

    [PROBLEM][SOLVED] - pair of socks count

---
+++ Problems/sockMerchant.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs = []
+    pair_count = 0;
+    for element in ar:
+        if element in pairs:
+            pair_count += 1
+            pairs.remove(element)
+            continue
+        pairs.append(element);
+
+    return pair_count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
