0b67549e4c43c2cbf18d128b85b2017141b8e019
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-09-23 00:20:51 +0530

    [DOC][UPDATE] - Static Typing

---
+++ Documentation/staticTyping.py
@@ -0,0 +1,12 @@
+# //for functions
+def addition(a: int, b: int) -> int:
+    return a + b
+
+
+addition(4, 10)
+
+# For Variables or attributes
+name: str = 'test'
+age: int = 10
+rating: float = 1.11
+is_exist: bool = True
