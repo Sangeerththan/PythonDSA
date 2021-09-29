2f9c8a06578f1bfd645f992443074e5d96958ca6
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-09-29 13:40:28 +0530

    [PROBLEM][SOLVED] - Number Pattern for a Rhombus

---
+++ Problems/general/NumberPattern.py
@@ -0,0 +1,39 @@
+# Reading number of rows
+row = int(input('Enter how many lines? '))
+
+# Generating pattern
+def firsthalf(rows):
+    for i in range(1, rows + 1):
+
+        # for space
+        for j in range(1, rows + 1 - i):
+            print(' ', end='')
+
+        # for increasing pattern
+        for j in range(1, i + 1):
+            print(j, end='')
+
+        # for decreasing pattern
+        for j in range(i - 1, 0, -1):
+            print(j, end='')
+
+        # Moving to next line
+        print()
+def bottomHalf(rows):
+    for i in range(rows,0,-1):
+
+        # for space
+        for j in range(1, rows + 2 - i):
+            print(' ', end='')
+
+        # for increasing pattern
+        for j in range(1, i + 1):
+            print(j, end='')
+
+        # for decreasing pattern
+        for j in range(i - 1, 0, -1):
+            print(j, end='')
+            # Moving to next line
+        print()
+firsthalf((row//2)+1)
+bottomHalf(row//2)
