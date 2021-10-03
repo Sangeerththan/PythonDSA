c71ad2856003965246729c2bd8f6bf87c9e42892
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-10-04 00:05:58 +0530

    [ALGORITHM][PENDING] - Friends pairing

---
+++ Algorithms/DynamicProgramming/FriendsPairing.py
@@ -0,0 +1,15 @@
+def countFriendsPairings(n):
+    dp = [0 for i in range(n + 1)]
+
+    for i in range(n + 1):
+
+        if (i <= 2):
+            dp[i] = i
+        else:
+            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2]
+
+    return dp[n]
+
+
+n = 4
+print(countFriendsPairings(n))
