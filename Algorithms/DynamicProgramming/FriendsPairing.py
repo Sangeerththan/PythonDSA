9137149942b15f997f37e16f8e92b29e6f95f009
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-10-12 00:29:58 +0530

    [ALGORITHM][PENDING] - Tiling  With Dominos

--- Algorithms/DynamicProgramming/FriendsPairing.py
+++ Algorithms/DynamicProgramming/FriendsPairing.py
@@ -1,15 +1,16 @@
-def countFriendsPairings(n):
-    dp = [0 for i in range(n + 1)]
-
-    for i in range(n + 1):
-
-        if (i <= 2):
-            dp[i] = i
-        else:
-            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2]
-
-    return dp[n]
+import cmath
+from collections import Callable
 
 
-n = 4
-print(countFriendsPairings(n))
+def laguerre_method(f_0: Callable, f_1: Callable, f_2: Callable, x0: float, n: float, epsilon: float = 0.00001):
+    xk = x0
+    while abs(f_0(xk)) > epsilon:
+        G = f_1(xk) / f_0(xk)
+        H = G ** 2 - f_2(xk) / f_0(xk)
+        root = cmath.sqrt((n - 1) * (n * H - G ** 2))
+        d = max(abs(G + root), abs(G - root))
+        a = n / d
+        xk -= a
+    return xk
+
+print(laguerre_method(f_0, f_1, f_2, 6, 3))
