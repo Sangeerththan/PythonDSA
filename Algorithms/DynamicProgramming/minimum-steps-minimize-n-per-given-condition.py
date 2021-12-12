a02e977cf2aa66aa286386112b01972d964fdb7d
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-12-12 10:23:58 +0530

    [ALGORITHM][PENDING] - Time Complexity: O(n), as there will be n unique calls.
    Space Complexity: O(n)

---
+++ Algorithms/DynamicProgramming/minimum-steps-minimize-n-per-given-condition.py
@@ -0,0 +1,20 @@
+def getMinSteps(n):
+    table = [0] * (n + 1)
+
+    for i in range(n + 1):
+        table[i] = n - i
+
+    for i in range(n, 0, -1):
+
+        if not (i % 2):
+            table[i // 2] = min(table[i] + 1, table[i // 2])
+
+        if not (i % 3):
+            table[i // 3] = min(table[i] + 1, table[i // 3])
+
+    return table[1]
+
+
+if __name__ == "__main__":
+    n = 14
+    print(getMinSteps(n))
