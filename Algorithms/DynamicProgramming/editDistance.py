0cf44a32e2be8751707f089537004b6c7b8fcc4e
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-12-15 08:54:16 +0530

    [ALGORITHM][PENDING] - Refactoring

--- Algorithms/DynamicProgramming/editDistance.py
+++ Algorithms/DynamicProgramming/editDistance.py
@@ -13,22 +13,23 @@ def minDis(s1, s2, n, m, dp):
             dp[n][m] = dp[n - 1][m - 1]
             return dp[n][m]
     else:
-        if (dp[n - 1][m] != -1):
+        if dp[n - 1][m] != -1:
             m1 = dp[n - 1][m]
         else:
             m1 = minDis(s1, s2, n - 1, m, dp)
 
-        if (dp[n][m - 1] != -1):
+        if dp[n][m - 1] != -1:
             m2 = dp[n][m - 1]
         else:
             m2 = minDis(s1, s2, n, m - 1, dp)
-        if (dp[n - 1][m - 1] != -1):
+        if dp[n - 1][m - 1] != -1:
             m3 = dp[n - 1][m - 1]
         else:
             m3 = minDis(s1, s2, n - 1, m - 1, dp)
 
         dp[n][m] = 1 + min(m1, min(m2, m3))
         return dp[n][m]
+
 
 str1 = "voldemort"
 str2 = "dumbledore"
