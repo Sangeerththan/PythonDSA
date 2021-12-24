2631534ab75e1a874f053b023d5512aaa628ba7c
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-12-24 23:28:00 +0530

    [ALGORITHM][PENDING] - nth term of Stern’s Diatomic Series. Stern’s diatomic series is the sequence which generates the following integer sequence 0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, 2, 5, 3, 4, ……. It arises in the Calkin-Wilf tree. It is sometimes also known as the fusc function.

---
+++ Algorithms/DynamicProgramming/find-n-th-element-from-sterns-diatomic-series.py
@@ -0,0 +1,14 @@
+def findSDSFunc(n):
+    DP = [0] * (n + 1)
+    DP[0] = 0
+    DP[1] = 1
+    for i in range(2, n + 1):
+        if (int(i % 2) == 0):
+            DP[i] = DP[int(i / 2)]d n
+        else:
+            DP[i] = (DP[int((i - 1) / 2)]
+                     + DP[int((i + 1) / 2)])
+    return DP[n]
+
+n = 15
+print(findSDSFunc(n))
