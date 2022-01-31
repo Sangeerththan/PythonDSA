74300dda06a06fc27d31b9644c1f08c1d66af0f9
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2022-02-01 00:45:43 +0530

    [ALGORITHM][PENDING] - count(n)” be the count of ways to place tiles on a “n x 4” grid, f(n – 1 + m – 1) != \frac {(n - 1 + m - 1)!} {(n - 1) ! (m - 1)!}

---
+++ Algorithms/DynamicProgramming/count-number-of-ways-to-fill-a-n-x-4-grid-using-1-x-4-tiles.py
@@ -0,0 +1,15 @@
+def count(n):
+    dp = [0 for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        if i <= 3:
+            dp[i] = 1
+        elif i == 4:
+            dp[i] = 2
+        else:
+            dp[i] = dp[i - 1] + dp[i - 4]
+    return dp[n]
+
+
+n = 5
+print("Count of ways is"),
+print(count(n))
