2057f24f3a3eafab6d3338a6ec5d625c84ab72df
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2022-02-09 21:29:32 +0530

    [ALGORITHM][PENDING] - Kadane’s Algorithm can be viewed both as a greedy and DP

--- Algorithms/DynamicProgramming/count-possible-paths-top-left-bottom-right-nxm-matrix.py
+++ Algorithms/DynamicProgramming/count-possible-paths-top-left-bottom-right-nxm-matrix.py
@@ -1,6 +1,8 @@
 def numberOfPaths(m, n):
     for i in range(n, (m + n - 1)):
-        path *= i;
-        path //= (i - n + 1);
-    return path;
-print(numberOfPaths(3, 3));
+        path *= i
+        path //= (i - n + 1)
+    return path
+
+
+print(numberOfPaths(3, 3))
